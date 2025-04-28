11f4ab348e2c17f4bdc754b8847cb5dd104d0703
Kan <ssitinan@gmail.com>
2025-04-28 14:52:02 +0700

    update

--- Model/Arima/Arima.py
+++ Model/Arima/Arima.py
@@ -20,9 +20,10 @@ from statsmodels.tsa.api import VAR
 # ========================
 # 📂 2. Load Dataset
 # ========================
-DATA_PATH = os.path.join("../Dataset/GHG_cleaned_v1.csv")
-GHG_DATA = pd.read_csv(DATA_PATH)
-print("🔹 Columns:", GHG_DATA.columns)
+DATA_PATH = os.path.join("..", "..", "AvailableData", "GHG_cleaned_v1.csv")
+df = pd.read_csv(DATA_PATH)
+print("🔹 Columns:", df.columns)
+
 
 # ========================
 # 🧹 3. Data Cleaning & Overview
