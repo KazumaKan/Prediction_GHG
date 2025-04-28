96a1dfe059b4bfd8a1d643bd8ad1fb95cd05907f
Kan <ssitinan@gmail.com>
2025-04-28 16:12:48 +0700

    ไม้แก้แล้ว

---
+++ Model/version.py
@@ -0,0 +1,22 @@
+import sklearn
+import pandas as pd
+import numpy as np
+import matplotlib
+import xgboost
+import joblib
+import scipy
+import statsmodels
+import pmdarima as pm
+import seaborn as sns
+
+# ตรวจสอบเวอร์ชันของแต่ละไลบรารี
+print(f"scikit-learn version: {sklearn.__version__}")
+print(f"pandas version: {pd.__version__}")
+print(f"numpy version: {np.__version__}")
+print(f"matplotlib version: {matplotlib.__version__}")
+print(f"xgboost version: {xgboost.__version__}")
+print(f"joblib version: {joblib.__version__}")
+print(f"scipy version: {scipy.__version__}")
+print(f"statsmodels version: {statsmodels.__version__}")
+print(f"pmdarima version: {pm.__version__}")
+print(f"seaborn version: {sns.__version__}")
